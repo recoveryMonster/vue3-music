94c138920eb0a4a2008401f9b5673e4b8a0fc21b
recoveryMonster <fengsongting@gmail.com>
2021-02-25 23:32:26 +0800

    feat: list-view 列表组件

---
+++ src/components/ListView/ListView.vue
@@ -0,0 +1,85 @@
+<template>
+  <Scroll
+    :data="data"
+    class="list-view_wrapper"
+  >
+    <ul>
+      <li
+        v-for="group in data"
+        :key="group.title.slice(0,1)"
+        class="list-group"
+      >
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li
+            v-for="singer in group.list"
+            :key="singer.id"
+            class="list-group-item"
+          >
+            <img
+              class="avatar"
+              v-lazy="singer.avatar"
+              alt="avatar"
+            >
+            <span class="name">{{singer.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </Scroll>
+</template>
+
+<script>
+import Scroll from 'components/Scroll/Scroll.vue';
+export default {
+  components: {
+    Scroll
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => ([])
+    }
+  },
+  setup () {
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.list-view_wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: $color-background;
+  .list-group {
+    padding-bottom: 30px;
+    .list-group-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background-color: $color-highlight-background;
+    }
+    .list-group-item {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+}
+</style>
--- src/views/singer/singer.vue
+++ src/views/singer/singer.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    硌手页面
+  <div class="singer-page">
+    <ListView :data="singerList"></ListView>
   </div>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import ListView from 'components/ListView/ListView.vue'
 export default {
+  components: {
+    ListView
+  },
   setup () {
     const store = useStore()
     store.dispatch('singer/getSingerList')
@@ -22,4 +26,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.singer-page {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
