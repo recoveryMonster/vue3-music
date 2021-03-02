f5289d00146dbd830ef600551dfd37be0542ab64
recoveryMonster <fengsongting@gmail.com>
2021-03-02 23:38:11 +0800

    feat: musicList 歌手背景及标题设置

---
+++ src/components/MusicList/MusicList.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="music-lsit_wrapper">
+    <div class="back">
+      <i class="icon-back"></i>
+    </div>
+    <h1 class="title">{{title}}</h1>
+    <div
+      class="bg-image"
+      :style="bgStyle"
+    >
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+  props: {
+    songList: {
+      type: Array,
+      default: () => ([]),
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    bgImage: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props) {
+    const bgStyle = computed(() => `background-image: url(${props.bgImage})`)
+
+    return {
+      bgStyle
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/styles/mixin.scss';
+.music-lsit_wrapper {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: $color-background;
+  .back {
+    position: absolute;
+    top: 0;
+    left: 6px;
+    z-index: 50;
+    .icon-back {
+      display: block;
+      padding: 10px;
+      font-size: $font-size-large-x;
+      color: $color-theme;
+    }
+  }
+  .title {
+    position: absolute;
+    top: 0;
+    left: 10%;
+    z-index: 40;
+    width: 80%;
+    @include no-wrap;
+    text-align: center;
+    line-height: 40px;
+    font-size: $font-size-large;
+    color: $color-text;
+  }
+  .bg-image {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 70%;
+    transform-origin: top;
+    background-size: cover;
+  }
+}
+</style>
--- src/views/singer/SingerDetail.vue
+++ src/views/singer/SingerDetail.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="singer-detail_wrapper">
-    {{JSON.stringify(songList)}}
+    <MusicList
+      :songList="songList"
+      :title="title"
+      :bgImage="bgImage"
+    ></MusicList>
   </div>
 </template>
 
 <script>
+import MusicList from 'components/MusicList/MusicList.vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { computed, onMounted } from 'vue'
 export default {
   name: 'SingerDetail',
+  components: {
+    MusicList
+  },
   setup () {
     const store = useStore()
     const router = useRouter()
 
     const songList = computed(() => store.getters['singer/songList'])
     const selectedSinger = computed(() => store.state.singer.selectedSinger)
+    const title = computed(() => selectedSinger.value?.name)
+    const bgImage = computed(() => selectedSinger.value?.avatar)
     onMounted(() => {
       const singerId = selectedSinger.value?.id
       if (!singerId) {
@@ -26,7 +36,9 @@ export default {
     })
     return {
       selectedSinger,
-      songList
+      songList,
+      title,
+      bgImage
     }
   }
 }
