ea2c603528bb82f1f6f9f092c599635cb390c5e0
recoveryMonster <fengsongting@gmail.com>
2021-03-14 00:54:33 +0800

    feat: 播放页展开收起动画处理

--- src/views/player/player.vue
+++ src/views/player/player.vue
@@ -3,115 +3,196 @@
     class="player"
     v-show="playList.length"
   >
-    <div
-      class="normal-player"
-      v-show="fullScreen"
+    <transition
+      name="normal"
+      @enter="transitionEnter"
+      @after-enter="transitionAfterEnter"
+      @leave="transitionLeave"
+      @after-leave="transitionAfterLeave"
     >
-      <div class="background">
-        <img
-          alt="bg-image"
-          :src="currentSong.image"
-        >
-      </div>
-      <div class="top">
-        <div
-          class="back"
-          @click="changeFullScreen(false)"
-        >
-          <i class="icon-back"></i>
+      <div
+        class="normal-player"
+        v-show="fullScreen"
+      >
+        <div class="background">
+          <img
+            alt="bg-image"
+            :src="currentSong.image"
+          >
         </div>
-        <h1
-          class="title"
-          v-html="currentSong.name"
-        ></h1>
-        <h2
-          class="subtitle"
-          v-html="currentSong.singer"
-        ></h2>
-      </div>
-      <div class="middle">
-        <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
-              <img
-                :src="currentSong.image"
-                class="image"
-              >
+        <div class="top">
+          <div
+            class="back"
+            @click="changeFullScreen(false)"
+          >
+            <i class="icon-back"></i>
+          </div>
+          <h1
+            class="title"
+            v-html="currentSong.name"
+          ></h1>
+          <h2
+            class="subtitle"
+            v-html="currentSong.singer"
+          ></h2>
+        </div>
+        <div class="middle">
+          <div class="middle-l">
+            <div
+              class="cd-wrapper"
+              ref="cdWrapper"
+            >
+              <div class="cd">
+                <img
+                  :src="currentSong.image"
+                  class="image"
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i class="icon-play"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-not-favorite"></i>
             </div>
           </div>
         </div>
       </div>
-      <div class="bottom">
-        <div class="operators">
-          <div class="icon i-left">
-            <i class="icon-sequence"></i>
-          </div>
-          <div class="icon i-left">
-            <i class="icon-prev"></i>
-          </div>
-          <div class="icon i-center">
-            <i class="icon-play"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon-next"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon icon-not-favorite"></i>
-          </div>
+    </transition>
+    <transition name="mini">
+
+      <div
+        class="mini-player"
+        v-show="!fullScreen"
+        @click="changeFullScreen(true)"
+      >
+        <div class="icon">
+          <img
+            swidth="40"
+            height="40"
+            :src="currentSong.image"
+          >
+        </div>
+        <div class="text">
+          <h2
+            class="name"
+            v-html="currentSong.name"
+          ></h2>
+          <p
+            class="desc"
+            v-html="currentSong.singer"
+          ></p>
+        </div>
+        <div class="control">
+          <i class="icon-playlist"></i>
         </div>
       </div>
-    </div>
-    <div
-      class="mini-player"
-      v-show="!fullScreen"
-      @click="changeFullScreen(true)"
-    >
-      <div class="icon">
-        <img
-          swidth="40"
-          height="40"
-          :src="currentSong.image"
-        >
-      </div>
-      <div class="text">
-        <h2
-          class="name"
-          v-html="currentSong.name"
-        ></h2>
-        <p
-          class="desc"
-          v-html="currentSong.singer"
-        ></p>
-      </div>
-      <div class="control">
-
-      </div>
-      <div class="control">
-        <i class="icon-playlist"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import { useStore } from 'vuex'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { types } from '@/store/mutation-types'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from '@/utils/dom'
+const transform = prefixStyle('transform')
 export default {
   setup () {
     const store = useStore()
+    const cdWrapper = ref(null)
 
     const currentSong = computed(() => store.getters['song/currentSong'])
     const playList = computed(() => store.state.song.playList)
     const fullScreen = computed(() => store.state.song.fullScreen)
 
     const changeFullScreen = (isFull) => store.commit(`song/${types.UPDATE_FULL_SCREEN}`, isFull)
-
+    const transitionEnter = (el, done) => {
+      const { x, y, scale } = getPosAndScale()
+      const animation = {
+        // 0%的时候，图片先缩小 显示在左下角
+        0: {
+          transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+        },
+        // 60% 的时候，回到原位 图片放大
+        60: {
+          transform: `translate3d(0,0,0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0,0,0) scale(1)`
+        }
+      }
+      // 注册动画
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+      // 运行动画
+      animations.runAnimation(cdWrapper.value, 'move', done)
+    }
+    const transitionAfterEnter = () => {
+      animations.unregisterAnimation('move')
+      if (cdWrapper.value) {
+        cdWrapper.value.style.animation = ''
+      }
+    }
+    const transitionLeave = (el, done) => {
+      if (cdWrapper.value) {
+        cdWrapper.value.style.transition = 'all 0.4s'
+        const { x, y, scale } = getPosAndScale()
+        cdWrapper.value.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+        cdWrapper.value.addEventListener('transitionend', done)
+      }
+    }
+    const transitionAfterLeave = () => {
+      if (cdWrapper.value) {
+        cdWrapper.value.style.animation = ''
+        cdWrapper.value.style[transform] = ''
+      }
+    }
+    const getPosAndScale = () => {
+      const paddingBottom = 30
+      const paddingTop = 80
+      const paddingLeft = 40
+      const targetWidth = 40
+      const width = window.innerWidth * 0.8
+      const scale = targetWidth / width
+      const x = -(window.innerWidth / 2 - paddingLeft)
+      const y = window.innerHeight - paddingBottom - paddingTop - width / 2
+      return {
+        x,
+        y,
+        scale
+      }
+    }
     return {
       playList,
       fullScreen,
       currentSong,
-      changeFullScreen
+      cdWrapper,
+      changeFullScreen,
+      transitionEnter,
+      transitionLeave,
+      transitionAfterEnter,
+      transitionAfterLeave
     }
   }
 }
@@ -251,6 +332,24 @@ export default {
         .icon-favorite {
           color: $color-sub-theme;
         }
+      }
+    }
+    &.normal-enter-active,
+    &.normal-leave-active {
+      transition: all 0.4s;
+      .top,
+      .bottom {
+        transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32);
+      }
+    }
+    &.normal-enter,
+    &.normal-leave-to {
+      opacity: 0;
+      .top {
+        transform: translate3d(0, -100px, 0);
+      }
+      .bottom {
+        transform: translate3d(0, 100px, 0);
       }
     }
   }
